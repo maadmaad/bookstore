ef11b7a4ba691155dceb1ef844e01d285367b96a
Grzegorz Fula <grzegorz.fula@gmail.com>
2022-01-07 15:42:09 +0100

    12 application.properties

--- src/main/java/pl/fula/bookstore/bookstore/AppInit.java
+++ src/main/java/pl/fula/bookstore/bookstore/AppInit.java
@@ -1,6 +1,7 @@
 package pl.fula.bookstore.bookstore;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.fula.bookstore.bookstore.catalog.application.CatalogController;
@@ -9,13 +10,23 @@ import pl.fula.bookstore.bookstore.catalog.domain.Book;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class AppInit implements CommandLineRunner {
     private final CatalogController catalogController;
+    private final String title;
+    private final Long limit;
+
+    // TODO 12 - properties. (values in application.properties)
+    //          ':' after properties is a default value (if proprty does not exist)
+    public AppInit(CatalogController catalogController, @Value("${bookstore.catalog.booktitle}") String title,
+                   @Value("${bookstore.catalog.limit:2}") Long limit) {
+        this.catalogController = catalogController;
+        this.title = title;
+        this.limit = limit;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        List<Book> books = catalogController.findByTitle("Pan");
-        books.forEach(System.out::println);
+        List<Book> books = catalogController.findByTitle(title);
+        books.stream().limit(limit).forEach(System.out::println);
     }
 }
