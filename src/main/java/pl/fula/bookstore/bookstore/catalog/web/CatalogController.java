4a9ae15896614f55767c04dc5d28647f2bea572e
Grzegorz Fula <grzegorz.fula@gmail.com>
2022-01-08 18:11:46 +0100

    4.4 - ResponseEntity.ok(), ResponseEntity.notFound()

--- src/main/java/pl/fula/bookstore/bookstore/catalog/web/CatalogController.java
+++ src/main/java/pl/fula/bookstore/bookstore/catalog/web/CatalogController.java
@@ -1,6 +1,8 @@
 package pl.fula.bookstore.bookstore.catalog.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,9 @@ public class CatalogController {
     }
 
     @GetMapping("/{id}")
-    public Book getById(@PathVariable Long id) {
-        return catalog.findById(id).orElse(null);
+    public ResponseEntity<Book> getById(@PathVariable Long id) {
+        return catalog.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
