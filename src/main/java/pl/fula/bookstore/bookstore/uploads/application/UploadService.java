9d8df0b54f47e4267cfc480104bf5c054e059902
Grzegorz Fula <grzegorz.fula@gmail.com>
2022-01-09 21:06:43 +0100

    4.13 - serving files

--- src/main/java/pl/fula/bookstore/bookstore/uploads/application/UploadService.java
+++ src/main/java/pl/fula/bookstore/bookstore/uploads/application/UploadService.java
@@ -7,6 +7,7 @@ import pl.fula.bookstore.bookstore.uploads.domain.Upload;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -15,7 +16,7 @@ public class UploadService implements UploadUseCase {
 
     @Override
     public Upload save(SaveUploadCommand command) {
-        String newId = RandomStringUtils.randomAlphabetic(8);
+        String newId = RandomStringUtils.randomAlphabetic(8).toLowerCase();
         Upload upload = new Upload(
                 newId,
                 command.getBytes(),
@@ -26,5 +27,10 @@ public class UploadService implements UploadUseCase {
         storage.put(upload.getId(), upload);
         System.out.println("Upload saved: " + upload.getFilename() + " with id: " + newId);
         return upload;
+    }
+
+    @Override
+    public Optional<Upload> getById(String id) {
+        return Optional.ofNullable(storage.get(id));
     }
 }
